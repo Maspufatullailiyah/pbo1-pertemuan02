63f185ce6e495315f174d02ca374dd34e5149f9a
Maspufatullailiyah <maspufa@gmail.com>
2020-04-14 05:36:43 -0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+ System.out.println("== Data Mahasiswa ===========================");
+ System.out.println("NPM   : 18630133");
+ System.out.println("Nama  : Maspufatullailiyah");
+ System.out.println("Kelas : 4A Non Reguler Banjarbaru");
+ System.out.println("FTI, Uniska Univ");
+ System.out.println("Instagram : maspufa32");
+ System.out.println("=============================================");
+ }
+}
